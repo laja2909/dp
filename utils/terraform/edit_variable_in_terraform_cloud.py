1c55977ffc7c1cc059d8c5c865f0077dc0065d88
Laja2909 <lauri2029@gmail.com>
2025-03-18 23:39:35 +0200

    added function to change local_ip variable

--- utils/terraform/edit_variable_in_terraform_cloud.py
+++ utils/terraform/edit_variable_in_terraform_cloud.py
@@ -1,53 +1,77 @@
 import requests
 import json
-from helper import get_env_variable
+from helper import get_env_variable, get_public_ip_address
 
 # get variable list from the workspace
 # get id of the specified variable
 # edit variable
 
-def get_response_content(response:requests.Response):
-    if  response.status_code >= 200 and response.status_code < 300:
+def get_response_content(response:requests.Response) -> json:
+    if  response.status_code == 200:
         print("retrieved the response..")
-        content = response.content
+        content = response.json()
     else:
-        print("Did not get the required response")
-        content = None
+        raise Exception
     return content
 
-def get_workspace_id(organization_name:str)-> None:
+def get_workspace_id_for_organization(organization_name:str, workspace_name:str)-> None:
     tf_token = get_env_variable('TF_TOKEN')
     headers = {'Authorization': 'Bearer '+tf_token,
                'Content-Type': 'application/vnd.api+json'}
     tf_organizations_endpoint = f'https://app.terraform.io/api/v2/organizations/{organization_name}/workspaces'
     tf_response = requests.request("GET", tf_organizations_endpoint, headers=headers)
-    print(tf_response)
     content = get_response_content(tf_response)
-    return content['data']['id']
+    for ind, value in enumerate(content['data']):
+        if value['attributes']['name']==workspace_name:
+            workspace_id = value['id']
+        else:
+            continue
+    return workspace_id
+
+def get_variable_id_of_workspace(organization_name:str,workspace_name:str, variable_name:str)-> None:
+    tf_token = get_env_variable('TF_TOKEN')
+    headers = {'Authorization': 'Bearer '+tf_token,
+               'Content-Type': 'application/vnd.api+json'}
+    tf_workspace_id = get_workspace_id_for_organization(organization_name,workspace_name)
+
+    tf_workspace_vars_endpoint = f'https://app.terraform.io/api/v2/workspaces/{tf_workspace_id}/vars'
+    tf_response = requests.request("GET", tf_workspace_vars_endpoint, headers=headers)
+    content = get_response_content(tf_response)
+    for ind, value in enumerate(content['data']):
+        if value['attributes']['key']==variable_name:
+            variable_id = value['id']
+        else:
+            continue
+    return variable_id
     
 
 
-def change_value_of_tf_variable(variable_name:str)-> None:
+def edit_tf_local_ip_variable(organization_name:str,workspace_name:str)-> None:
     tf_token = get_env_variable('TF_TOKEN')
-    tf_organization='dhub-dev'
+    tf_local_ip_variable_name = 'local_ip'
     # get workspaces and their ids
     headers = {'Authorization': 'Bearer '+tf_token,
                'Content-Type': 'application/vnd.api+json'}
     
-    tf_organizations_endpoint = f'https://app.terraform.io/api/v2/organizations/{tf_organization}/workspaces'
-    tf_response = requests.request("GET", tf_organizations_endpoint, headers=headers)
-    tf_response.content['data']['id']
+    tf_variable_id = get_variable_id_of_workspace(organization_name,workspace_name,tf_local_ip_variable_name)
+    my_public_ip_address = get_public_ip_address()
+    # we need to change tf local ip variable into current public ip address
+    payload = {"data": {
+                "id":tf_variable_id,
+                "attributes": {
+                    "key":tf_local_ip_variable_name,
+                    "value":my_public_ip_address,
+                    "description": "description",
+                    "category":"terraform",
+                    "hcl": False,
+                    "sensitive": False
+                },
+                "type":"vars"
+                }
+            }
+    tf_variable_end_point=f'https://app.terraform.io/api/v2/vars/{tf_variable_id}'
+    requests.request("PATCH", tf_variable_end_point, headers=headers,data=json.dumps(payload))
 
-   
-
-    """
-    tf_variable = variable_name
-    
-    
-    workspace_vars_endpoint = f'https://app.terraform.io/api/v2/workspaces/{tf_workspace}/vars'
-    # get variable list from the workspace
-    tf_vars_response = requests.request("GET", workspace_vars_endpoint, headers=headers)
-    """
 
 if __name__=='__main__':
-    print(get_workspace_id('dhub-dev'))
+    edit_tf_local_ip_variable('dhub-dev','DHub')
--- utils/terraform/helper.py
+++ utils/terraform/helper.py
@@ -1,4 +1,15 @@
 import os
+import socket
+import requests
 
 def get_env_variable(name):
     return os.environ[name]
+
+def get_local_ip_address():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
+
+def get_public_ip_address():
+    public_ip = requests.get('https://api.ipify.org').text
+    return public_ip
