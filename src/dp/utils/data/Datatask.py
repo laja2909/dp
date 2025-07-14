6723a80cef7180c53cae458a5d27a33e2bf6b5a9
Laja2909 <lauri2029@gmail.com>
2025-07-14 21:40:11 +0300

    some utils

--- src/dp/utils/data/Datatask.py
+++ src/dp/utils/data/Datatask.py
@@ -1,6 +1,10 @@
 class DataSource:
-    def __init__(self,name:str):
+    def __init__(self,name:str, type:str):
         self.name = name
+        self._type = type
+
+    def get_type(self):
+        return self._type
 
 class DataTask:
     def __init__(self,name:str):
@@ -11,4 +15,12 @@ class DataTask:
         self._data_source = data_source
     def add_data_target(self,data_source:DataSource):
         self._data_target = data_source
+
+    def set_task_type(self,task_type:str):
+        #incremental
+        #full refresh
+        self._task_type = task_type
+    def get_task_type(self):
+        return self._task_type
+
 
